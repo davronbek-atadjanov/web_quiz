5bd0a4dfe017c9185b3cb6cac23f38cea80e6294
Davronbek <atadjanovdavronbek863@gmail.com>
2025-01-20 18:19:49 +0500

    feat(new version quiz web application

--- core/settings.py
+++ core/settings.py
@@ -70,34 +70,34 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-#
-
-
-# Supabase'dan olingan PostgreSQL URL
-POSTGRES_URL = config('POSTGRES_URL_NON_POOLING')
-
-# URLni parchalash
-url = urlparse(POSTGRES_URL)
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': url.path[1:],  # URLdagi '/postgres' qismidan keyingi qism
-        'USER': url.username,
-        'PASSWORD': url.password,
-        'HOST': url.hostname,
-        'PORT': url.port,
-        'OPTIONS': {
-            'sslmode': 'require',  # SSL rejimini yoqish
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+
+# # Supabase'dan olingan PostgreSQL URL
+# POSTGRES_URL = config('POSTGRES_URL_NON_POOLING')
+#
+# # URLni parchalash
+# url = urlparse(POSTGRES_URL)
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': url.path[1:],  # URLdagi '/postgres' qismidan keyingi qism
+#         'USER': url.username,
+#         'PASSWORD': url.password,
+#         'HOST': url.hostname,
+#         'PORT': url.port,
+#         'OPTIONS': {
+#             'sslmode': 'require',  # SSL rejimini yoqish
+#         },
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
